d98aa0fe1426cf9101c3b7a27ff7f0eeb084c9fd
Sim <sim.neodin@gmail.com>
2022-09-03 15:15:30 +0200

    start

--- index.php
+++ index.php
@@ -3,3 +3,15 @@
 phpinfo();
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello World!</title>
+</head>
+<body>
+
+<h1>Hello world!</h1>
+<p>This is the first file in my new Git Repo.</p>
+
+</body>
+</html>
